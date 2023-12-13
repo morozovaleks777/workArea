291d86a627103df76c5f0c3d25d4b3792f56ee15
oleksandr.o.morozov <morozovaleks@ukr.net>
2023-12-13 21:01:06 +0200

    Committing changes before applying stash

--- app/src/main/java/com/morozov/workarea/di/NetworkModule.kt
+++ app/src/main/java/com/morozov/workarea/di/NetworkModule.kt
@@ -1,15 +1,19 @@
 package com.morozov.workarea.di
 
-import com.apollographql.apollo.ApolloClient
+
 import com.apollographql.apollo3.ApolloClient
+import com.morozov.workarea.data.repositoryImpl.ArticleRepositoryImpl
+import com.morozov.workarea.data.repositoryImpl.ReaderPassRepositoryImpl
+import com.morozov.workarea.data.repositoryImpl.ShopRepositoryImpl
+import com.morozov.workarea.data.repositoryImpl.TagRepositoryImpl
+import com.morozov.workarea.domain.ArticleRepository
+import com.morozov.workarea.domain.ReaderPassRepository
+import com.morozov.workarea.domain.ShopRepository
+import com.morozov.workarea.domain.TagRepository
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
-import okhttp3.OkHttpClient
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
-import retrofit2.converter.scalars.ScalarsConverterFactory
 import javax.inject.Singleton
 
 
@@ -17,21 +21,37 @@ import javax.inject.Singleton
 @InstallIn(SingletonComponent::class)
 class NetworkModule {
 
-//    // ApolloModule.kt
-//    @Module
-//    @InstallIn(SingletonComponent::class)
-//    object ApolloModule {
-
-        @Provides
-        @Singleton
-        fun provideApolloClient(): ApolloClient {
-            return ApolloClient.builder()
-                .serverUrl("https://v2server.dailywire.com/discussion/graphql")
-                .build()
-        }
-
-
+    @Provides
+    @Singleton
+    fun provideApolloClient(): ApolloClient {
+        return ApolloClient.Builder()
+            .serverUrl("https://v2server.dailywire.com/article/graphql")
+            .build()
     }
 
 
-//}
+    @Provides
+    @Singleton
+    fun provideTagRepository(tagRepositoryImpl: TagRepositoryImpl): TagRepository {
+        return tagRepositoryImpl
+    }
+
+    @Provides
+    @Singleton
+    fun provideArticleRepository(articleRepositoryImpl: ArticleRepositoryImpl): ArticleRepository {
+        return articleRepositoryImpl
+    }
+    @Provides
+    @Singleton
+    fun provideShopRepository(shopRepositoryImpl: ShopRepositoryImpl): ShopRepository {
+        return shopRepositoryImpl
+    }
+    @Provides
+    @Singleton
+    fun provideReaderPassRepository(readerPassRepositoryImpl: ReaderPassRepositoryImpl): ReaderPassRepository {
+        return readerPassRepositoryImpl
+    }
+
+}
+
+
